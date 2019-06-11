3d90476f6c71c7bf47a473f45ada8b4575fe44a3
Mark Lawrence <mlawrence@alsde.edu>
2019-06-11 16:57:13 -0500

    update routing add courses controller

--- Courses.Web/Controllers/CourseController.cs
+++ Courses.Web/Controllers/CourseController.cs
@@ -1,47 +1,20 @@
-﻿using Courses.Infrastructure;
-using System.Data.Entity;
-using System.Threading.Tasks;
-using System.Web.Http;
-using AutoMapper.QueryableExtensions;
-using Courses.Core.Dtos;
-using DevExtreme.AspNet.Data;
-using DevExtreme.AspNet.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Courses.Web.Controllers
 {
-    [RoutePrefix("api/courses")]
-    public class CourseController : ApiController
+    [RoutePrefix("courses")]
+    public class CourseController : Controller
     {
-        private readonly CourseDbContext _context;
-
-        public CourseController()
+        [Route]
+        public ActionResult Index()
         {
-            _context = CourseDbContext.Create();
+            return View();
         }
 
-        [HttpGet, Route("")]
-        public async Task<object> Get(DataSourceLoadOptions loadOptions)
+        [Route("{courseCode}")]
+        public ActionResult Detail(string courseCode)
         {
-            var list = await _context.CourseViews.ToListAsync();
-
-            return Ok(DataSourceLoader.Load(list, loadOptions));
-        }
-
-        [HttpGet, Route("{id}")]
-        public async Task<object> Get(int id)
-        {
-            var dto = await _context.CourseViews.FirstOrDefaultAsync(c => c.Id == id);
-
-            return Ok(dto);
-        }
-
-        [HttpGet, Route("{id}/full"), Authorize]
-        public async Task<object> GetFull(int id)
-        {
-            var dto = await _context.Courses
-                .Include(x => x.ProgramCourses).ProjectTo<CourseDto>()
-                .FirstOrDefaultAsync(c => c.Id == id);
-            return Ok(dto);
+            return View((object)courseCode);
         }
 
     }
---
+++ Courses.Web/Controllers/api/CourseController.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper.QueryableExtensions;
+using Courses.Core.Dtos;
+using Courses.Infrastructure;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+
+namespace Courses.Web.Controllers.api
+{
+    [RoutePrefix("api/courses")]
+    public class CourseController : ApiController
+    {
+        private readonly CourseDbContext _context;
+
+        public CourseController()
+        {
+            _context = CourseDbContext.Create();
+        }
+
+        [HttpGet, Route("")]
+        public async Task<object> Get(DataSourceLoadOptions loadOptions)
+        {
+            var list = await _context.CourseViews.ToListAsync();
+
+            return Ok(DataSourceLoader.Load(list, loadOptions));
+        }
+
+        [HttpGet, Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            var dto = await _context.CourseViews.FirstOrDefaultAsync(c => c.Id == id);
+
+            return Ok(dto);
+        }
+
+        [HttpGet, Route("{id}/full"), Authorize]
+        public async Task<object> GetFull(int id)
+        {
+            var dto = await _context.Courses
+                .Include(x => x.ProgramCourses).ProjectTo<CourseDto>()
+                .FirstOrDefaultAsync(c => c.Id == id);
+            return Ok(dto);
+        }
+
+    }
+}
