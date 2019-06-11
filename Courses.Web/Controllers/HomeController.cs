16f4f2be7158a50a35aaf61c7b37d150777e9d4d
Mark Lawrence <mlawrence@alsde.edu>
2019-06-11 14:22:24 -0500

    bootstrap and vendor bundle init layout

--- Courses.Web/Controllers/HomeController.cs
+++ Courses.Web/Controllers/HomeController.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Courses.Web.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
 
+        [Route("about")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +18,7 @@ namespace Courses.Web.Controllers
             return View();
         }
 
+        [Route("contact")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
