5af679cc4ae26951f290ac34e5afe371f78d6c18
Mark Lawrence <mlawrence@alsde.edu>
2019-06-11 13:22:33 -0500

    add course controller

---
+++ Courses.Web/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using Courses.Infrastructure;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Courses.Web.Controllers
+{
+    [RoutePrefix("api/courses")]
+    public class CourseController : ApiController
+    {
+        private readonly CourseDbContext _context;
+
+        public CourseController()
+        {
+            _context = CourseDbContext.Create();
+        }
+
+        [HttpGet, Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            var dto = await _context.CourseViews.FirstOrDefaultAsync(c => c.Id == id);
+
+            return Ok(dto);
+        }
+
+    }
+}
