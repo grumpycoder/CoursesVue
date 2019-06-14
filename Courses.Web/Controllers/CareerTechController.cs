01316a18a38ff5455196d300fd9a86576680f269
Mark Lawrence <mlawrence@alsde.edu>
2019-06-14 11:14:48 -0500

    add career tech controller and route for clusters

---
+++ Courses.Web/Controllers/CareerTechController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace Courses.Web.Controllers
+{
+    [RoutePrefix("careertech")]
+    public class CareerTechController : Controller
+    {
+        [Route]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("clusters")]
+        public ActionResult Clusters()
+        {
+            return View();
+        }
+
+    }
+}
