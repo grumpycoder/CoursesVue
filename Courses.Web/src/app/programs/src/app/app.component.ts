b8bbc656626aa17a48b0dc75addba0be7301080e
Mark <grumpycoder@outlook.com>
2019-06-26 07:20:52 -0500

    add delete credentials from program

--- Courses.Web/src/app/programs/src/app/app.component.ts
+++ Courses.Web/src/app/programs/src/app/app.component.ts
@@ -1,31 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
+  templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
-  title = 'programs';
+export class AppComponent implements OnInit {
+
+  title = 'Career Tech Programs';
+  programs: any;
+  program: any;
+  schoolYears: any;
+  programTypes: any;
+  clusters: any;
+  cachedProgram: any;
+  programCreds: any;
+  courses: any;
+  programCourses: any;
+  credentials: any;
+  selectedCredential: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get("api/ref/programs").subscribe(data => {
+      this.programs = data;
+    });
+    this.http.get('api/ref/programtypes').subscribe(data => {
+      this.programTypes = data;
+    });
+    this.http.get('api/ref/schoolyears').subscribe(data => {
+      this.schoolYears = data;
+    });
+
+    this.http.get("api/ref/clusters").subscribe(data => {
+      this.clusters = data;
+    });
+
+    this.http.get("api/ref/credentials").subscribe(data => {
+      this.credentials = data;
+    });
+  }
+
+  onSelectionChanged(item) {
+
+    this.http.get(`api/careertech/programs/${item.programCode}/edit`).subscribe(data => {
+      this.program = data;
+      this.cachedProgram = Object.assign({}, this.program);
+
+      this.http.get(`api/careertech/programs/${item.programCode}/credentials`).subscribe(data => {
+        this.programCreds = data;
+      })
+
+      this.http.get(`api/careertech/programs/${item.programCode}/courses`).subscribe(data => {
+        this.programCourses = data;
+      })
+    })
+
+  }
+
+  onSubmit(form) {
+    if (this.program.id == null) {
+      this.http.post('api/careertech/programs', this.program).subscribe(data => {
+        this.program = data;
+        this.cachedProgram = data;
+      })
+    } else {
+      this.http.put('api/careertech/programs', this.program).subscribe(data => {
+        this.cachedProgram = data;
+      })
+    }
+  }
+
+  cancel(form) {
+    form.reset(this.cachedProgram);
+    this.program = Object.assign({}, this.cachedProgram)
+  }
+
+  create() {
+    this.program = {
+      id: null
+    };
+  }
+
+  addCred(list) {
+    console.log('list', list.selectedItem);
+    var url = `api/careertech/programs/${this.program.programCode}/credential/${list.selectedItem.credentialCode}`;
+
+    this.http.post(url, null).subscribe(data => {
+      this.programCreds.push(data)
+    })
+  }
+
+  deleteCred(item) {
+    var url = `api/careertech/programs/${this.program.programCode}/credential/${item.itemData.credentialCode}`;
+    console.log('item', item.itemData);
+
+    this.http.delete(url).subscribe(data => {
+      console.log(`deleted ${item.itemData.credentialCode}`);
+    })
+
+  }
 }
